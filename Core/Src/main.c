07619802f353febb818912b8130c6855fc0aee9f
LAPTOP-BBT167US\ASUS <hao.truongbk04102204@hcmut.edu.vn>
2024-10-31 17:29:24 +0700

    init_lab_2_ex2

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -189,26 +189,56 @@ int main(void)
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   setTimer1(500);
+  setTimer2(1000);
   int led_status = 0;
   while (1)
   {
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-	  if(timer1_flag == 1 && led_status ==0){
-		  setTimer1(500);
-		  HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, GPIO_PIN_RESET);
-		  HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, GPIO_PIN_SET);
-		  display7SEG(1);
-		  led_status = 1;
+	  if(timer2_flag == 1){
+		  setTimer2(1000);
+		  HAL_GPIO_TogglePin(DOT_GPIO_Port, DOT_Pin);
 	  }
-	  else if(timer1_flag == 1 && led_status ==1){
-		  setTimer1(500);
-		  HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, GPIO_PIN_SET);
-		  HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, GPIO_PIN_RESET);
-		  display7SEG(2);
-		  led_status = 0;
+	  if(timer1_flag == 1){
+		  if(led_status==0){
+			  setTimer1(500);
+			  HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, GPIO_PIN_RESET);
+			  HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, GPIO_PIN_SET);
+			  HAL_GPIO_WritePin(EN0_GPIO_Port, EN2_Pin, GPIO_PIN_SET);
+			  HAL_GPIO_WritePin(EN0_GPIO_Port, EN3_Pin, GPIO_PIN_SET);
+			  display7SEG(1);
+			  led_status = 1;
+		  }
+		  else if(led_status==1){
+			  setTimer1(500);
+			  HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, GPIO_PIN_SET);
+			  HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, GPIO_PIN_RESET);
+			  HAL_GPIO_WritePin(EN0_GPIO_Port, EN2_Pin, GPIO_PIN_SET);
+			  HAL_GPIO_WritePin(EN0_GPIO_Port, EN3_Pin, GPIO_PIN_SET);
+			  display7SEG(2);
+			  led_status = 2;
+		  }
+		  else if(led_status==2){
+			  setTimer1(500);
+			  HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, GPIO_PIN_SET);
+			  HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, GPIO_PIN_SET);
+			  HAL_GPIO_WritePin(EN0_GPIO_Port, EN2_Pin, GPIO_PIN_RESET);
+			  HAL_GPIO_WritePin(EN0_GPIO_Port, EN3_Pin, GPIO_PIN_SET);
+			  display7SEG(3);
+			  led_status = 3;
+		  }
+		  else if(led_status==3){
+			  setTimer1(500);
+			  HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, GPIO_PIN_SET);
+			  HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, GPIO_PIN_SET);
+			  HAL_GPIO_WritePin(EN0_GPIO_Port, EN2_Pin, GPIO_PIN_SET);
+			  HAL_GPIO_WritePin(EN0_GPIO_Port, EN3_Pin, GPIO_PIN_RESET);
+			  display7SEG(0);
+			  led_status = 0;
+		  }
 	  }
+
   }
   /* USER CODE END 3 */
 }
@@ -310,14 +340,17 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, LED_RED_Pin|EN0_Pin|EN1_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, DOT_Pin|LED_RED_Pin|EN0_Pin|EN1_Pin
+                          |EN2_Pin|EN3_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, SEG0_Pin|SEG1_Pin|SEG2_Pin|SEG3_Pin
                           |SEG4_Pin|SEG5_Pin|SEG6_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : LED_RED_Pin EN0_Pin EN1_Pin */
-  GPIO_InitStruct.Pin = LED_RED_Pin|EN0_Pin|EN1_Pin;
+  /*Configure GPIO pins : DOT_Pin LED_RED_Pin EN0_Pin EN1_Pin
+                           EN2_Pin EN3_Pin */
+  GPIO_InitStruct.Pin = DOT_Pin|LED_RED_Pin|EN0_Pin|EN1_Pin
+                          |EN2_Pin|EN3_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
