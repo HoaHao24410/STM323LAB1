b23dc7c477309979f996d447090c2f985aa156a7
LAPTOP-BBT167US\ASUS <hao.truongbk04102204@hcmut.edu.vn>
2024-10-31 13:24:43 +0700

    init_stm32_lap2ex1

---
+++ Core/Inc/software_timer.h
@@ -0,0 +1,16 @@
+/*
+ * timer.h
+ *
+ *  Created on: Oct 23, 2024
+ *      Author: ASUS
+ */
+
+#ifndef INC_SOFWARE_TIMER_H_
+#define INC_SOFWARE_TIMER_H_
+
+#include "main.h"
+extern int timer1_flag;
+void setTimer1(int duration);
+void timerRun();
+
+#endif /* INC_SOFWARE_TIMER_H_ */
