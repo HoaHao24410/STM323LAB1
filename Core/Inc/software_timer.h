ff3f41de0598b9fba4be211545c60fc8a3faed65
LAPTOP-BBT167US\ASUS <hao.truongbk04102204@hcmut.edu.vn>
2024-11-30 13:48:02 +0700

    init for lab3

---
+++ Core/Inc/software_timer.h
@@ -0,0 +1,13 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Nov 30, 2024
+ *      Author: ASUS
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+#include "main.h"
+
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,6 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "software_timer.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
